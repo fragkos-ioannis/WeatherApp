00dcc0c673cb47077451fea590885e508e6f3028
Ioannis Fragkos <fragkos.io@gmail.com>
2024-07-10 12:52:36 +0300

    change project structure

---
+++ client/index.js
@@ -0,0 +1,136 @@
+import { elements } from './utilities/elements.js';
+import { changeBackgroundImage, updateDateTime } from './utilities/functions.js';
+
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const base = `http://localhost:3000/api/weather?lat=${lat}&lon=${long}`;
+
+            fetch(base)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server error: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    const { temp, feels_like, temp_min, temp_max, pressure, humidity, sea_level } = data.main;
+                    const place = data.name;
+                    const { speed, deg } = data.wind;
+                    const visibility = data.visibility;
+                    const { all } = data.clouds;
+                    const { description, icon } = data.weather[0];
+                    const { sunrise, sunset } = data.sys;
+
+                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                    const sunriseGMT = new Date(sunrise * 1000);
+                    const sunsetGMT = new Date(sunset * 1000);
+
+                    elements.country.textContent = place;
+                    elements.mainTemp.textContent = Math.round(temp);
+                    elements.disc.textContent = description;
+                    elements.tempHigh.textContent = Math.round(temp_max);
+                    elements.tempLow.textContent = Math.round(temp_min);
+                    elements.windSpeed.textContent = `${Math.round(speed * 3.6)} km/h`;
+                    elements.windDir.textContent = `Direction: ${deg}°`;
+                    elements.hum.textContent = `${humidity}%`;
+                    elements.realFeel.textContent = Math.round(feels_like);
+                    elements.vis.textContent = `${visibility} m`;
+                    elements.press.textContent = `${pressure} hPa`;
+                    elements.clouds.textContent = `${all}%`;
+                    elements.seaLevel.textContent = `${sea_level} hPa`;
+                    elements.sunRise.textContent = `Rise: ${sunriseGMT.toLocaleTimeString()}`;
+                    elements.sunSet.textContent = `Set: ${sunsetGMT.toLocaleTimeString()}`;
+                    elements.weatherIconImg.src = iconUrl;
+
+                    elements.noData.style.display = 'none';
+                    elements.weatherIconImg.style.display = 'block';
+                    elements.countryElement.style.top = 0;
+                    elements.form.style.bottom = 0;
+                    elements.form.style.top = '1%';
+                    changeBackgroundImage(description);
+                })
+        }, (error) => {
+            console.error('Geolocation error:', error);
+        });
+    }
+});
+
+elements.searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const location = elements.searchInput.value;
+
+    const geoRequestUrl = `http://localhost:3000/api/geocode?location=${encodeURIComponent(location)}`;
+
+    try {
+        const geocodingResponse = await fetch(geoRequestUrl);
+        const geocodingData = await geocodingResponse.json();
+        //console.log('Geocoding data fetched:', geocodingData);
+
+        if (geocodingData.results.length > 0) {
+            const { lat, lng } = geocodingData.results[0].geometry;
+
+            const weatherApiUrl = `http://localhost:3000/api/weather?lat=${lat}&lon=${lng}`;
+            const weatherResponse = await fetch(weatherApiUrl);
+            const weatherData = await weatherResponse.json();
+            //console.log('Weather data fetched:', weatherData);
+
+            if (weatherData) {
+                const { temp, feels_like, temp_min, temp_max, pressure, humidity, sea_level } = weatherData.main;
+                const place = weatherData.name;
+                const { speed, deg } = weatherData.wind;
+                const visibility = weatherData.visibility;
+                const { all } = weatherData.clouds;
+                const { description, icon } = weatherData.weather[0];
+                const { sunrise, sunset } = weatherData.sys;
+
+                const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                const sunriseGMT = new Date(sunrise * 1000);
+                const sunsetGMT = new Date(sunset * 1000);
+
+                elements.country.textContent = place;
+                elements.mainTemp.textContent = Math.round(temp);
+                elements.disc.textContent = description;
+                elements.tempHigh.textContent = Math.round(temp_max);
+                elements.tempLow.textContent = Math.round(temp_min);
+                elements.windSpeed.textContent = `${Math.round(speed * 3.6)} km/h`;
+                elements.windDir.textContent = `Direction: ${deg}°`;
+                elements.hum.textContent = `${humidity}%`;
+                elements.realFeel.textContent = Math.round(feels_like);
+                elements.vis.textContent = `${visibility} m`;
+                elements.press.textContent = `${pressure} hPa`;
+                elements.clouds.textContent = `${all}%`;
+                elements.seaLevel.textContent = `${sea_level} hPa`;
+                elements.sunRise.textContent = `Rise: ${sunriseGMT.toLocaleTimeString()}`;
+                elements.sunSet.textContent = `Set: ${sunsetGMT.toLocaleTimeString()}`;
+                elements.weatherIconImg.src = iconUrl;
+
+                elements.noData.style.display = 'none';
+                elements.weatherIconImg.style.display = 'block';
+                elements.countryElement.style.top = 0;
+                elements.form.style.bottom = 0;
+                elements.form.style.top = '1%';
+                changeBackgroundImage(description);
+            } else {
+                console.log('Weather data NOT found.');
+            }
+        } else {
+            console.log('No results found');
+        }
+    } catch (error) {
+        console.error('Error fetching location data:', error);
+    }
+});
+// Update the date and time immediately
+updateDateTime();
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
---
+++ client/utilities/functions.js
@@ -0,0 +1,51 @@
+const precipitationDescriptions = ['shower rain', 'rain', 'snow', 'mist'];
+const cloudDescriptions = ['broken clouds', 'overcast clouds'];
+const sunnyDescription = ['few clouds', 'clear sky', 'scattered clouds'];
+
+//Change the backgorund image depending on the weather Description.
+export const changeBackgroundImage = (description) => {
+    const body = document.querySelector('body');
+    const isNight = document.querySelector('#status');
+
+    let imgUrl = '';
+    if (precipitationDescriptions.includes(description)) {
+        imgUrl = '../public/img/rain.jpg';
+    } else if (cloudDescriptions.includes(description)) {
+        imgUrl = '../public/img/alotcloud.jpg';
+    } else if (sunnyDescription.includes(description)) {
+        imgUrl = '../public/img/sunny.jpg';
+    } else {
+        imgUrl = '../public/img/sunny.jpg';
+    }
+
+    if (isNight == 'Night') {
+        body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${imgUrl})`;
+    } else {
+        body.style.backgroundImage = `url(${imgUrl})`;
+    }
+};
+
+//Show users' dateTime information.
+export function updateDateTime() {
+    const now = new Date();
+
+    // Formatting the date as '21-July-2023'
+    const day = now.getDate();
+    const month = now.toLocaleString('default', { month: 'long' });
+    const year = now.getFullYear();
+    const formattedDate = `${day}-${month}-${year}`;
+    document.getElementById('month').textContent = formattedDate;
+
+    // Formatting the day and time as 'Friday, 12:44pm'
+    const weekday = now.toLocaleString('default', { weekday: 'long' });
+    const hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    const hour12 = hours % 12 || 12; // Convert 24-hour format to 12-hour format
+    const formattedTime = `${weekday}, ${hour12}:${minutes}${ampm}`;
+    document.getElementById('day-time').textContent = formattedTime;
+
+    // Determining the day/night status
+    const status = hours >= 6 && hours < 18 ? 'Day' : 'Night';
+    document.getElementById('status').textContent = status;
+}
